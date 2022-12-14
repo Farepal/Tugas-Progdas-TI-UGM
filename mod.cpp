96b09fbbb73ebcba341afa2ffb1f2f554c62371d
Farepal <73487439+Farepal@users.noreply.github.com>
2022-12-14 13:47:22 +0700

    Add files via upload

---
+++ mod.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void deklarasi(int n, int angka[])
+{
+    for (int i = 0; i < n; i++)
+        cin >> angka[i];
+}
+
+void tampilkanGanjil(int n, int angka[])
+{
+    for (int i = 0; i < n; i++)
+        if (angka[i] % 2 == 1)
+            cout << angka[i] << " ";
+}
+
+int main()
+{
+    int angka[9999];
+    int n;
+    cout << "Masukkan jumlah angka : ";
+    cin >> n;
+    deklarasi(n, angka);
+    tampilkanGanjil(n, angka);
+}
